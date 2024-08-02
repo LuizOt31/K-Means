0c35c907f192ed685cf1447f37c8f50dc399cdf4
LuizOt31 <109004895+LuizOt31@users.noreply.github.com>
2024-08-01 22:43:25 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,34 +1,35 @@
 import pandas as pd
 import numpy as np
+from sklearn.cluster import k_means
 
-def selecao_grupo_de_cada_dado(df, k_grupos, k):
+def cluster_de_cada_dado(df, k_grupos, k):
     distancias = []
     grupo_cada_dado = []
 
     for i in range(df.shape[0]):
 
         for j in range(k):
-            distancias.append((np.linalg.norm(df.iloc[i] - k_grupos[j]), j))
+            distancias.append((np.linalg.norm(df.iloc[i] - k_grupos[j]), j)) # distância euclidiana de cada dado para cada grupo
 
         distancias.sort(key=lambda x: x[0])
 
         grupo_cada_dado.append(distancias[0][1])
 
         distancias = []
-
     return grupo_cada_dado
 
-
 def calcula_centroide(df, grupo_cada_dado, k):
-    k_grupos_novo = [[0] * df.shape[1]] * k
-    aux_total_objetos_cada_grupo = [0] * k
+    k_grupos_novo = [[0] * df.shape[1] for _ in range(k)] # Vetor com as novas posições dos centróides 
+    aux_total_objetos_cada_grupo = [0] * k # Vetor contendo o número de dados em cada cluster (indice do vetor == numero do grupo)
 
     for i in range(df.shape[0]):
         aux_total_objetos_cada_grupo[grupo_cada_dado[i]] += 1
 
         objeto = df.iloc[i]
+
         for j in range(df.shape[1]):
             k_grupos_novo[grupo_cada_dado[i]][j] += objeto.iloc[j]
+
 
     for i in range(k):
         for j in range(df.shape[1]):
@@ -37,25 +38,36 @@ def calcula_centroide(df, grupo_cada_dado, k):
     return k_grupos_novo  
 
 
-def k_means(df, k_grupos):
+def k_means(df, k_grupos, n_interation=10):
+    """
+    parâmetros:
+        * df -> dataframe (escolho fazer esse codigo com o dataframe do pandas do que com um numpy array como o sklearn)
+        * k_grupos -> array com a posição inicial dos grupos 
+        * n_interation -> número de interações de cálculo dos centróides. Por default será 10
+
+    Esse código não é tão complexo e há de ser melhorado conforme o tempo!
+    """
+    
     k = len(k_grupos)
-    k_grupo_novo = []
     grupo_cada_dado = []
 
-    grupo_cada_dado = selecao_grupo_de_cada_dado(df, k_grupos, k)
-    k_grupo_novo = calcula_centroide(df, grupo_cada_dado, k)
+    for i in range(n_interation):
+        grupo_cada_dado = cluster_de_cada_dado(df, k_grupos, k)
+        k_grupos = calcula_centroide(df, grupo_cada_dado, k)
 
-    return k_grupo_novo
+
+    return k_grupos
 
 
 def main():
     df = pd.DataFrame({
-    'x': [1, 2, 2, 15, 11, 12],
-    'y': [1, 1, 3, 14, 12, 14]   
+    'x': [1, 10, 4, 10, 20, 10],
+    'y': [2, 4, 6, 12, 14, 8]   
     })
 
-    k_g = [[4, 4], [13, 13]]
+    k_g = [[13, 13], [4, 4]]
 
-    print(k_means(df, k_g))
+    print(k_means(df, k_g, n_interation=5))
+
 
 main()
